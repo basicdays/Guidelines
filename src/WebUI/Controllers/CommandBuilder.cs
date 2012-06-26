dc1143e90d5506182db08c6d09eea4a98fb72d03
TJBach <thomas.j.bach@gmail.com>
2012-06-25 20:28:12 -0500

    Forgot to add some files in the last commit.

---
+++ src/WebUI/Controllers/CommandBuilder.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Web.Mvc;
+using Guidelines.Core;
+using Guidelines.WebUI.ActionResults;
+using Guidelines.WebUI.Errors;
+
+namespace Guidelines.WebUI.Controllers
+{
+	public class CommandBuilder
+	{
+		public CommandBuilder<TInputModel> Handle<TInputModel>()
+		{
+			return new CommandBuilder<TInputModel>();
+		}
+	}
+
+	public class CommandBuilder<TInputModel>
+	{
+		public CommandBuilderStageTwo<TInputModel> OnSuccesExecuteResult(Func<ActionResult> onSuccess)
+		{
+			return new CommandBuilderStageTwo<TInputModel>(onSuccess);
+		}
+
+		public CommandBuilderStageTwo<TInputModel> OnSuccesExecuteResult(ActionResult actionResult)
+		{
+			return new CommandBuilderStageTwo<TInputModel>(() => actionResult);
+		}
+	}
+
+	public class CommandBuilderStageTwo<TInputModel>
+	{
+		private readonly Func<ActionResult> _success;
+
+		public CommandBuilderStageTwo(Func<ActionResult> success)
+		{
+			_success = success;
+		}
+
+		public CommandBuilderStageThree<TInputModel> UseSuccessPathOnFailure()
+		{
+			return new CommandBuilderStageThree<TInputModel>(_success,
+				(input, mapper, error) => _success());
+		}
+
+		public CommandBuilderStageThree<TInputModel> OnFailureExecuteResult<TMapFrom>(Func<TMapFrom, ActionResult> onFailure)
+		{
+			return new CommandBuilderStageThree<TInputModel>(_success,
+				(input, mapper, error) => onFailure(mapper.Map<TInputModel, TMapFrom>(input)));
+		}
+
+		public CommandBuilderStageThree<TInputModel> OnFailureExecuteResult(Func<TInputModel, ActionResult> onFailure)
+		{
+			return new CommandBuilderStageThree<TInputModel>(_success,
+				(input, mapper, error) => onFailure(input));
+		}
+
+		public CommandBuilderStageThree<TInputModel> OnFailureHandleError(Func<ErrorContext, ActionResult> onFailure)
+		{
+			return new CommandBuilderStageThree<TInputModel>(_success,
+				(input, mapper, error) => onFailure(error));
+		}
+
+		public CommandBuilderStageThree<TInputModel> OnFailureExecuteResult(Func<ActionResult> onFailure)
+		{
+			return new CommandBuilderStageThree<TInputModel>(_success,
+				(input, mapper, error) => onFailure());
+		}
+
+		public CommandBuilderStageThree<TInputModel> OnFailureExecuteResult(ActionResult actionResult)
+		{
+			return new CommandBuilderStageThree<TInputModel>(_success,
+				(input, mapper, error) => actionResult);
+		}
+	}
+
+	public class CommandBuilderStageThree<TInputModel>
+	{
+		private Func<IGenericMapper, TInputModel> _message;
+		private readonly Func<ActionResult> _success;
+		private readonly Func<TInputModel, IGenericMapper, ErrorContext, ActionResult> _failure;
+
+		public CommandBuilderStageThree(Func<ActionResult> success, Func<TInputModel, IGenericMapper, ErrorContext, ActionResult> failure)
+		{
+			_success = success;
+			_failure = failure;
+		}
+
+		private CommandBuilderStageThree<TInputModel> WithMessage(TInputModel command)
+		{
+			_message = mapper => command;
+			return this;
+		}
+
+		private CommandBuilderStageThree<TInputModel> WithMessage<TMapFrom>(TMapFrom message)
+		{
+			_message = mapper => mapper.Map<TMapFrom, TInputModel>(message);
+			return this;
+		}
+
+		#region Command Execution
+
+		private CommandResult<TInputModel> Run()
+		{
+			return new CommandResult<TInputModel>(_message, _success, _failure);
+		}
+
+		public CommandResult<TInputModel> ExecuteWith(TInputModel command)
+		{
+			return WithMessage(command).Run();
+		}
+
+		public CommandResult<TInputModel> ExecuteWith<TMapFrom>(TMapFrom message)
+		{
+			return WithMessage(message).Run();
+		}
+
+		#endregion
+	}
+}
---
+++ src/WebUI/Controllers/QueryBuilder.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Web.Mvc;
+using Guidelines.Core;
+using Guidelines.WebUI.ActionResults;
+using Guidelines.WebUI.Errors;
+
+namespace Guidelines.WebUI.Controllers
+{
+	public class QueryBuilder
+	{
+		public QueryBuilder<TInputModel> Handle<TInputModel>()
+		{
+			return new QueryBuilder<TInputModel>();
+		}
+	}
+
+	public class QueryBuilder<TInput>
+	{
+		public QueryBuilder<TInput, TResult> WithResult<TResult>()
+		{
+			return new QueryBuilder<TInput, TResult>();
+		}
+	}
+
+	public class QueryBuilder<TInputModel, TResult>
+	{
+		public QueryBuilderStageTwo<TInputModel, TResult> OnSuccesExecuteResult(Func<TResult, IGenericMapper, ActionResult> onSuccess)
+		{
+			return new QueryBuilderStageTwo<TInputModel, TResult>(onSuccess);
+		}
+
+		public QueryBuilderStageTwo<TInputModel, TResult> OnSuccesExecuteResult(Func<TResult, ActionResult> onSuccess)
+		{
+			return new QueryBuilderStageTwo<TInputModel, TResult>((result, mapper) => onSuccess(result));
+		}
+
+		public QueryBuilderStageTwo<TInputModel, TResult> OnSuccesExecuteResult<TMappedResult>(Func<TMappedResult, ActionResult> onSuccess)
+		{
+			return new QueryBuilderStageTwo<TInputModel, TResult>((result, mapper) => onSuccess(mapper.Map<TResult, TMappedResult>(result)));
+		}
+
+		public QueryBuilderStageTwo<TInputModel, TResult> OnSuccesExecuteResult(Func<ActionResult> onSuccess)
+		{
+			return new QueryBuilderStageTwo<TInputModel, TResult>((result, mapper) => onSuccess());
+		}
+
+		public QueryBuilderStageTwo<TInputModel, TResult> OnSuccesExecuteResult(ActionResult actionResult)
+		{
+			return new QueryBuilderStageTwo<TInputModel, TResult>((result, mapper) => actionResult);
+		}
+	}
+
+	public class QueryBuilderStageTwo<TInputModel, TResult>
+	{
+		private readonly Func<TResult, IGenericMapper, ActionResult> _success;
+
+		public QueryBuilderStageTwo(Func<TResult, IGenericMapper, ActionResult> success)
+		{
+			_success = success;
+		}
+
+		public QueryBuilderStageThree<TInputModel, TResult> OnFailureExecuteResult<TMapFrom>(Func<TMapFrom, ActionResult> onFailure)
+		{
+			return new QueryBuilderStageThree<TInputModel, TResult>(_success,
+				(input, mapper, error) => onFailure(mapper.Map<TInputModel, TMapFrom>(input)));
+		}
+
+		public QueryBuilderStageThree<TInputModel, TResult> OnFailureExecuteResult(Func<TInputModel, ActionResult> onFailure)
+		{
+			return new QueryBuilderStageThree<TInputModel, TResult>(_success,
+				(input, mapper, error) => onFailure(input));
+		}
+
+		public QueryBuilderStageThree<TInputModel, TResult> OnFailureHandleError(Func<ErrorContext, ActionResult> onFailure)
+		{
+			return new QueryBuilderStageThree<TInputModel, TResult>(_success,
+				(input, mapper, error) => onFailure(error));
+		}
+
+		public QueryBuilderStageThree<TInputModel, TResult> OnFailureExecuteResult(Func<ActionResult> onFailure)
+		{
+			return new QueryBuilderStageThree<TInputModel, TResult>(_success,
+				(input, mapper, error) => onFailure());
+		}
+
+		public QueryBuilderStageThree<TInputModel, TResult> OnFailureExecuteResult(ActionResult actionResult)
+		{
+			return new QueryBuilderStageThree<TInputModel, TResult>(_success,
+				(input, mapper, error) => actionResult);
+		}
+	}
+
+	public class QueryBuilderStageThree<TInputModel, TResult>
+	{
+		private Func<IGenericMapper, TInputModel> _message;
+
+		private readonly Func<TResult, IGenericMapper, ActionResult> _success;
+		private readonly Func<TInputModel, IGenericMapper, ErrorContext, ActionResult> _failure;
+
+		public QueryBuilderStageThree(Func<TResult, IGenericMapper, ActionResult> success, Func<TInputModel, IGenericMapper, ErrorContext, ActionResult> failure)
+		{
+			_success = success;
+			_failure = failure;
+		}
+
+		private QueryBuilderStageThree<TInputModel, TResult> WithMessage(TInputModel command)
+		{
+			_message = mapper => command;
+			return this;
+		}
+
+		private QueryBuilderStageThree<TInputModel, TResult> WithMessage<TMapFrom>(TMapFrom message)
+		{
+			_message = mapper => mapper.Map<TMapFrom, TInputModel>(message);
+			return this;
+		}
+
+		#region Query Execution
+
+		private QueryResult<TInputModel, TResult> Run()
+		{
+			return new QueryResult<TInputModel, TResult>(_message, _success, _failure);
+		}
+
+		public QueryResult<TInputModel, TResult> ExecuteWith(TInputModel command)
+		{
+			return WithMessage(command).Run();
+		}
+
+		public QueryResult<TInputModel, TResult> ExecuteWith<TMapFrom>(TMapFrom message)
+		{
+			return WithMessage(message).Run();
+		}
+
+		#endregion
+	}
+}
