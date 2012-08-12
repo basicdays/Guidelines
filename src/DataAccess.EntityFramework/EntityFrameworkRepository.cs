b5907c88b149e71d4f993f1e3b22e72970d2cc43
TJBach <thomas.j.bach@gmail.com>
2012-08-12 14:47:47 -0500

    Adding a renamed file.

---
+++ src/DataAccess.EntityFramework/EntityFrameworkRepository.cs
@@ -0,0 +1,62 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using Guidelines.Core;
+
+namespace Guidelines.DataAccess.EntityFramework
+{
+	public class EntityFrameworkRepository<TDomain> : IRepository<TDomain>
+		where TDomain : class 
+	{
+		private readonly IDbContext _dbContext;
+		private readonly IDbSet<TDomain> _set;
+
+		protected EntityFrameworkRepository(IDbContext dbContext)
+		{
+			_dbContext = dbContext;
+			_set = dbContext.Set<TDomain>();
+		}
+
+		protected IDbContext Context
+		{
+			get { return _dbContext; }
+		}
+
+		protected IDbSet<TDomain> Set
+		{
+			get { return _set; }
+		}
+
+		public IQueryable<TDomain> GetQueryableSet()
+		{
+			return Set;
+		}
+
+		public TDomain Insert(TDomain toInsert)
+		{
+			return Set.Add(toInsert);
+		}
+
+		public TDomain Update(TDomain toUpdate)
+		{
+			//EF tracks this for us.
+			return toUpdate;
+		}
+
+		public void Delete(TDomain toDelete)
+		{
+			Set.Remove(toDelete);
+		}
+	}
+
+	public class EntityFrameworkRepository<TDomain, TId> : EntityFrameworkRepository<TDomain>, IRepository<TDomain, TId>
+		where TDomain : class, IIdentifiable<TId>
+	{
+		protected EntityFrameworkRepository(IDbContext dbContext) 
+			: base(dbContext) {}
+
+		public TDomain GetById(TId id)
+		{
+			return Set.Find(id);
+		}
+	}
+}
